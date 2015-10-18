a4e191b196d7d0d5a55f4742cdb1692d43dea42d
Gabriel <sormgabriel@gmail.com>
2015-10-17 22:18:39 -0300

    Se testea que al agregar un barco al tablero deja de estar vacio

--- base_cucumber/model/tablero.rb
+++ base_cucumber/model/tablero.rb
@@ -1,3 +1,4 @@
+require_relative 'fila'
 class Tablero
 
   def initialize
@@ -11,8 +12,40 @@ class Tablero
   end
 
   def filas_estan_vacias?
+    estan_vacias = true
     @filas.each { |key, fila|
-      fila.esta_vacia?}
+      estan_vacias= estan_vacias &&  fila.esta_vacia?}
+    return estan_vacias
+  end
+
+  def ubicar_barco(un_barco, una_posicion_str, una_orientacion_str)
+    if una_orientacion_str == "horizontal"  
+      ubicar_barco_en_forma_horizontal(un_barco, una_posicion_str)
+    else
+      ubicar_barco_en_forma_vertical(un_barco, una_posicion_str)
+    end     
+     
+  end
+
+  def ubicar_barco_en_forma_horizontal(un_barco, una_posicion_str)
+    ubicar_barco_en_fila(@filas[una_posicion_str[1]],un_barco,una_posicion_str[0].to_i)
+  end
+
+  def ubicar_barco_en_fila(una_fila, un_barco, un_numero_de_celda)
+    una_fila.ubicar_barco(un_barco,un_numero_de_celda)
+    
+  end
+
+  def ubicar_barco_en_forma_vertical(un_barco, una_posicion_str)
+    ubicar_barco_en_columna(un_barco, una_posicion_str)      
+  end
+
+  def ubicar_barco_en_columna(un_barco, una_posicion_str)
+      fila_id=una_posicion_str[1]	
+      for i in 1..un_barco.tamanho
+          @filas[fila_id].ocupar_celda(una_posicion_str[0].to_i)
+          fila_id= fila_id.next
+      end
   end
 
 end
--- base_cucumber/spec/tablero_spec.rb
+++ base_cucumber/spec/tablero_spec.rb
@@ -1,6 +1,6 @@
 require 'rspec'
 require_relative '../model/tablero'
-
+require_relative '../model/destructor'
 describe 'tablero de batalla naval' do
 
   describe 'inicializacion del tablero' do
@@ -12,5 +12,14 @@ describe 'tablero de batalla naval' do
     end 
 
   end
+  describe 'ocupacion del tablero' do
+  
+    it 'cuando se agrega un barco al tablero deja de estar vacio' do
 
+      tablero = Tablero.new 
+      destructor = Destructor.new
+      tablero.ubicar_barco(destructor, "1A", "horizontal")
+      expect(tablero.esta_vacio?).to be_falsey
+    end
+  end
 end
