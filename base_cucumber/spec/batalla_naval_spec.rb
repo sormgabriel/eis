0742b4f073565a936fdff8d1ab587ff72f82c7e2
Gabriel <sormgabriel@gmail.com>
2015-10-19 02:11:09 -0300

    Se testea que al poner un barco destructor en una posicion ocupada por un crucero deberia lanzar un error

--- base_cucumber/spec/batalla_naval_spec.rb
+++ base_cucumber/spec/batalla_naval_spec.rb
@@ -12,12 +12,19 @@ describe 'Batalla Naval' do
 
   end
   describe 'agregando barcos' do
+      let(:batalla_naval){BatallaNaval.new}
 
     it 'se puede ubicar un barco destructor en la posicion 1A de forma horizontal' do
-      batalla_naval = BatallaNaval.new
       batalla_naval.poner_barco("destructor","1A","horizontal")
       expect(batalla_naval.se_pudo_ubicar_barco?("destructor","1A","horizontal")).to be_truthy
     end
+
+    it 'al ubicar un destructor en una posicion ya ocupada por un crucero debe lanzar una excepcion' do
+      batalla_naval.poner_barco("crucero","4A","vertical") 
+      expect{batalla_naval.poner_barco("destructor","4A","vertical")}.to raise_error 
+      
+    end
   end
+
 
 end
